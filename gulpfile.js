0269266c50e9b8f6dcd310820657a076e8746f31
MityaDV <mityadv@gmail.com>
2019-05-12 00:14:25 +0300

    добавляет плагины gulp-posthtml и posthtml-include, вставляет спрайт

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,8 @@ var rename = require("gulp-rename");
 var imagemin = require("gulp-imagemin");
 var webp = require("gulp-webp");
 var svgstore = require("gulp-svgstore");
+var posthtml = require("gulp-posthtml");
+var include = require("posthtml-include");
 
 gulp.task("css", function () {
   return gulp.src("source/sass/style.scss")
@@ -64,6 +66,14 @@ gulp.task("sprite", function () {
     }))
     .pipe(rename("sprite.svg"))
     .pipe(gulp.dest("source/img"));
+});
+
+gulp.task("html", function () {
+  return gulp.src("source/*.html")
+    .pipe(posthtml([
+      include()
+    ]))
+    .pipe(gulp.dest("source"));
 });
 
 gulp.task("start", gulp.series("css", "server"));
