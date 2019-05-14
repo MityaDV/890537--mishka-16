3cc1d11b5da309c70ef3faa55d923bfb719e9893
MityaDV <mityadv@gmail.com>
2019-05-14 17:22:10 +0300

    подключение векторных картинок главная

--- gulpfile.js
+++ gulpfile.js
@@ -68,7 +68,7 @@ gulp.task("webp", function () {
 });
 
 gulp.task("sprite", function () {
-  return gulp.src("source/img/icon-*.svg")
+  return gulp.src("source/img/*.svg" )
     .pipe(svgstore({
       inlineSvg: true
     }))
