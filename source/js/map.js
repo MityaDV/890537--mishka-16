9ec29bb0308b9ba54e7feefeaf8e7c94aa16a164
MityaDV <mityadv@gmail.com>
2019-05-25 12:01:43 +0300

    правки js и карты

--- source/js/map.js
+++ source/js/map.js
@@ -1,18 +1,15 @@
-
-document.addEventListener("DOMContentLoaded", function (event) {
   ymaps.ready(init);
-  var map;
+
   function init() {
-    map = new ymaps.Map("contacts-map", {
-      center: [59.764791, 30.4668216],
+    var map = new ymaps.Map("contacts-map", {
+      center: [59.93944115603922, 30.32302403991186],
       zoom: 12,
       controls: []
     });
-    var placemark = new ymaps.Placemark([59.764791, 30.4668216], {}, {
+    var placemark = new ymaps.Placemark([59.938633647616214, 30.32304549758399], {}, {
       iconLayout: "default#image",
       iconImageHref: "img/sprite-svg-icons/icon-map-pin.svg",
       iconImageSize: [67, 100]
     });
     map.geoObjects.add(placemark);
   }
-});
--- source/js/modal.js
+++ source/js/modal.js
@@ -1,21 +1,24 @@
-var offers = document.querySelector(".offers__button");
+var actionButtons = document.querySelectorAll(".js-modal");
 var modal = document.querySelector(".modal");
-var overlay = document.querySelector(".overlay");
-
-offers.addEventListener("click", function (evt) {
-  evt.preventDefault();
-  modal.classList.add("modal__open");
-  overlay.classList.add("overlay__open");
-});
-
-document.addEventListener("keyup", function (evt) {
+var modalOverlay = document.querySelector(".overlay");
+var onKeyUp = function (evt) {
   if (evt.keyCode === 27) {
     evt.preventDefault();
     if (modal.classList.contains("modal__open")) {
       modal.classList.remove("modal__open");
     }
-    if (overlay.classList.contains("overlay__open")) {
-      overlay.classList.remove("overlay__open");
+    if (modalOverlay.classList.contains("overlay__open")) {
+      modalOverlay.classList.remove("overlay__open");
     }
   }
-});
+  document.removeEventListener("keyup", onKeyUp);
+}
+
+for (var i = 0; i < actionButtons.length; i++) {
+  actionButtons[i].addEventListener("click", function (evt) {
+    evt.preventDefault();
+    modal.classList.add("modal__open");
+    modalOverlay.classList.add("overlay__open");
+    document.addEventListener("keyup", onKeyUp);
+  });
+}
